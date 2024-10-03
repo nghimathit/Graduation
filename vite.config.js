405805de713e582fd300a049e2c4b8273effbf0a
nghimathit <vannghinguyen152001@gmail.com>
2024-10-03 23:01:24 +0700

    post1

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Graduation/', 
   server: {
     host: true,
   },
-  base: '/Graduation/', 
+  
 })
 // cấu hình chạy netword
 
